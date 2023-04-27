3f9ea7efc2b0e6a980d06fc1ed37db24d31ab438
MichaelDulin <michaeldulin1211@gmail.com>
2023-04-27 00:35:07 +0000

    added CRUD

---
+++ handleUpdate/index.js
@@ -0,0 +1,21 @@
+const dynamoose = require("dynamoose");
+
+const countrySchema = new dynamoose.Schema({
+  name: String,
+  population: Number,
+  language: String,
+  id: String,
+});
+
+const CountryModel = dynamoose.model("Country", countrySchema);
+
+exports.handler = async (event) => {
+    console.log("CREATE COUNTRY EVENT OBJECT: ", event);
+  
+    const response = {
+
+    };
+  
+
+    return response;
+  };
